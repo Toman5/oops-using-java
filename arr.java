cbf57912de77bbe95e03f78af4c99e5d8b0ad33f
Toman5 <124652926+Toman5@users.noreply.github.com>
2023-07-21 13:57:54 +0530

    To check whether two or more arrays are equal

---
+++ arr.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class arr{
+public static void main(String[] args)
+{
+int arr1[]={1,2,3,4};
+int arr2[]={1,2,3,4};
+int arr3[]={1,2,3};
+System.out.printf("Array.equals(arr1[],arr2[])");
+System.out.printf("Array.equals(arr2[],arr3[])");
+}
+}
+
+
