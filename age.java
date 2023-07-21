4ccd08d299b24f073354a48092f8a6ec394b790f
Toman5 <124652926+Toman5@users.noreply.github.com>
2023-07-21 13:52:44 +0530

    Age of a child
    age

---
+++ age.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class age{
+public static void main(String[] args)
+{
+int age=2;
+String a = String.format("%d",age);
+String b = String.valueOf(age);
+System.out.println("The child is " + a + " years old");
+System.out.println("The child is " + a + " years old");
+}
+}
